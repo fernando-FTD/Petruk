b33572d639760a48e58922a2a9be58d929ccbcb2
fernando-FTD <fernandoramadhani0611@gmail.com>
2024-02-28 11:01:48 +0700

    Hello World
    membuat kode hello world.

---
+++ Pertemuan 2/array1.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    double arr1[9];
+    arr1[0] = 12.4;
+    arr1[1] = 5.42;
+    arr1[2] = 44.12;
+    arr1[3] = 98.1;
+    arr1[4] = 65.14;
+    arr1[5] = 66.5;
+    arr1[6] = 13.4;
+    arr1[7] = 71.2;
+    arr1[8] = 8.52;
+    
+    cout << arr1[0] << endl;
+    cout << arr1[1] << endl;
+    cout << arr1[2] << endl;
+    cout << arr1[3] << endl;
+    cout << arr1[4] << endl;
+    cout << arr1[5] << endl;
+    cout << arr1[6] << endl;
+    cout << arr1[7] << endl;
+    cout << arr1[8] << endl;
+
+
+
+
+    int arr2[12];
+    arr2[0] = 1;
+    arr2[1] = 2;
+    arr2[2] = 3;
+    arr2[3] = 4;
+    arr2[4] = 5;
+    arr2[5] = 6;
+    arr2[6] = 7;
+    arr2[7] = 8;
+    arr2[8] = 9;
+    arr2[9] = 10;
+    arr2[10] = 11;
+    arr2[11] = 12;
+
+    cout << arr2[0] << endl;
+    cout << arr2[1] << endl;
+    cout << arr2[2] << endl;
+    cout << arr2[3] << endl;
+    cout << arr2[4] << endl;
+    cout << arr2[5] << endl;
+    cout << arr2[6] << endl;
+    cout << arr2[7] << endl;
+    cout << arr2[8] << endl;
+    cout << arr2[9] << endl;
+    cout << arr2[10] << endl;
+    cout << arr2[11] << endl;
+
+    return 0;
+}
---
+++ Pertemuan 2/array2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    double arr1[9];
+    for (int i = 0; i < 9; i++)
+    {
+        cin >> arr1[i];
+    }
+    for (int j = 0; j < 9; j++)
+    {
+        cout << arr1[j] << endl;
+    }
+
+    int arr2[12];
+    for (int i = 0; i < 12; i++)
+    {
+        cin >> arr2[i];
+    }
+    for (int j = 0; j < 12; j++)
+    {
+        cout << arr2[j] << endl;
+    }
+    
+
+    return 0;
+}
